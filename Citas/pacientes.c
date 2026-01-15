e940a15f3061fd594d41e092d900dcb0e3892972
arielito-uio <miniguachitobluee@gmail.com>
2026-01-15 00:21:19 -0500

    Implementada funcion ExistenciaPaciente

--- Citas/pacientes.c
+++ Citas/pacientes.c
@@ -91,3 +91,21 @@ void listarPacientes() {
 	
 	fclose(archivo);
 }
+int existePaciente(char cedulaBuscada[]) {
+	FILE *file = fopen("data/pacientes.txt", "r");
+	if (!file) return 0;
+	
+	char cedula[15], nombre[50], telefono[15];
+	int edad;
+	
+	while (fscanf(file, "%[^;];%[^;];%d;%s\n",
+				  cedula, nombre, &edad, telefono) != EOF) {
+		if (strcmp(cedula, cedulaBuscada) == 0) {
+			fclose(file);
+			return 1;
+		}
+	}
+	
+	fclose(file);
+	return 0;
+}
