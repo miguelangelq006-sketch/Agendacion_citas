9a47f4af62437692a8dff073f8df87479dc79cc6
idkisma3l <galosalazar2005@gmail.com>
2026-01-15 10:21:04 -0500

    Implementar el módulo de citas, estados y reportes

--- Citas/citas.h
+++ Citas/citas.h
@@ -1,8 +1,11 @@
 #ifndef CITAS_H
 #define CITAS_H
 
-void asignarCita();
+void agendarCita();
 void cancelarCita();
 void reprogramarCita();
+void listarCitasPorMedico(int codigoMedico);
+void listarCitasPorPaciente(char cedula[]);
+int existeConflicto(int codigoMedico, char fecha[], char hora[]);
 
 #endif
